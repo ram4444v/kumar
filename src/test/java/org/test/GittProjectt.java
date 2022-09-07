c2703b06a5cfe6bbaefdb6ce4fb8514292c50c5b
ram4444v <vv8147106@gmail.com>
2022-09-07 11:15:46 +0530

    ram

---
+++ src/test/java/org/test/GittProjectt.java
@@ -0,0 +1,49 @@
+package org.test;
+
+public class GittProjectt {
+public static void main(String[] args) {
+	
+	System.out.println("project1");
+	System.out.println("project2");
+	System.out.println("project3");
+	System.out.println("project4");
+	System.out.println("project5");
+	System.out.println("project6");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
